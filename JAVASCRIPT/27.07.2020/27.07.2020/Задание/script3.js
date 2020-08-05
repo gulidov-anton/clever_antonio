324235978d7855707095fead11b4390fd01f59c2
Anton Hulidau <gulidov.antonio@gmail.com>
2020-08-05 23:59:32 +0300

    Изменил операторы с background на backgroundColor

--- JAVASCRIPT/27.07.2020/27.07.2020/Задание/script3.js
+++ JAVASCRIPT/27.07.2020/27.07.2020/Задание/script3.js
@@ -1,5 +1,5 @@
 let elements = document.querySelectorAll('ul > li');
-elements[0].style.background = 'blue';
-elements[1].style.background = 'green';
-elements[2].style.background = 'yellow';
-elements[3].style.background = 'orange';
+elements[0].style.backgroundColor = 'blue';
+elements[1].style.backgroundColor = 'green';
+elements[2].style.backgroundColor = 'yellow';
+elements[3].style.backgroundColor = 'orange';
