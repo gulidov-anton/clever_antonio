182f6415e7637aeaad18e28494ec3963fec244ca
Anton <gulidov.antonio@gmail.ru>
2021-05-03 13:14:01 +0300

    Заманил let на const, добавил точку с запятой, где пропустил

--- JAVASCRIPT/Fetch/task1/script.js
+++ JAVASCRIPT/Fetch/task1/script.js
@@ -1,27 +1,27 @@
 form.onsubmit = async (e) => {
     e.preventDefault();
     try {
-      let formData = new FormData(form);
-      let formDataObj = Array.from(formData.entries()).reduce(
+      const formData = new FormData(form);
+      const formDataObj = Array.from(formData.entries()).reduce(
         (prev, [name, value]) => ({
           ...prev,
           [name]: value,
         }),
         {}
       );
-      let formDataJSON = JSON.stringify(formDataObj);
+      const formDataJSON = JSON.stringify(formDataObj);
 
-      let response = await fetch('https://httpbin.org/post', {
+      const response = await fetch('https://httpbin.org/post', {
         method: 'POST',
         body: formDataJSON
       });
     
-        let result = await response.json();
+        const result = await response.json();
         alert(`Данные, переданные на сервер: ${result.data}`);
         form.hidden = true;
         document.body.insertAdjacentHTML('afterbegin','<div class="success">Данные загрузились!</div>');
     } catch {
-      email.insertAdjacentHTML('afterend', '<div class="error">ОШИБКА</div>')
-      pass.insertAdjacentHTML('afterend', '<div class="error">ОШИБКА</div>')
+      email.insertAdjacentHTML('afterend', '<div class="error">ОШИБКА</div>');
+      pass.insertAdjacentHTML('afterend', '<div class="error">ОШИБКА</div>');
     };
 };
